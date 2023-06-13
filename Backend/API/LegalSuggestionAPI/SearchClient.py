ff8de0bc23a455d3af6b886cb9ee292640ef86fc
Shoaib <shoaibakbarm@gmail.com>
2023-06-13 10:12:44 +0500

    removed print

--- Backend/API/LegalSuggestionAPI/SearchClient.py
+++ Backend/API/LegalSuggestionAPI/SearchClient.py
@@ -4,7 +4,6 @@ from qdrant_client import QdrantClient
 from Creds import *
 from typing import Dict, List
 
-
 class SearchClient:
 
     def __init__(
@@ -48,7 +47,7 @@ class SearchClient:
             search_result = self.search()
 
             context = search_result[0].payload["text"]
-            print(context)
+            # print(context)
 
             prompt = f"You are an AI Legal suggestion assistant providing helpful legal advice. You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.write a comprehensive suggestion according to Law and in the context provided.If there will be any other question like abuse language or greeting or appreciation to your work, answer strict or polite according to the question.  You should only provide hyperlinks that reference the context below.If there will be word india in question or answer then consider 'india' as 'pakistan' and don't write 'india' in your answer, replace it with 'pakistan' Do NOT make up hyperlinks.If you can't find the answer in the context below, just say 'Hmm, I'm not sure.' Don't try to make up an answer.If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context. \n\nContext:\n{context}\n\nQuery:\n{self.query}\nAnswer:\n"
 
