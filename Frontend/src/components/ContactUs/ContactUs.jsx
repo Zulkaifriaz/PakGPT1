4ab6175c4a98cf5f41d24cb4c0519c5bafd02f47
Zulkaif Riaz <90593596+Zulkaifriaz@users.noreply.github.com>
2023-12-26 00:03:59 +0500

    Update ContactUs.jsx

--- Frontend/src/components/ContactUs/ContactUs.jsx
+++ Frontend/src/components/ContactUs/ContactUs.jsx
@@ -9,12 +9,10 @@ const Contact = () => {
 
             <div className='flex flex-wrap'>
                 <div className='flex flex-col items-center px-6 justify-center text-center leading-8 text-lg font-light' >
-                    <p className='mt-8'>Welcome to our website, where we use artificial intelligence to help you navigate the complexities of the constitution. Our goal is to make it easier for you to understand your rights and responsibilities as a citizen, and to empower you to participate in the democratic process.
-
-                        Our AI-powered platform provides you with a range of tools and resources that can help you better understand the constitution. Whether you are a student, a lawyer, or just a concerned citizen, our platform has something for everyone.</p>
+                    <p className='mt-8'>Zulkaifriaz34@gmail.com</p>
                     <div className='mt-8 flex gap-4'>
                         <Link to='/' className='bg-primary text-secondary font-medium px-4 rounded-full py-2 hover:tracking-wider transition-all'>Get Started</Link>
-                        <Link to='/video' className='px-4 py-2 border-primary border text-primary rounded-full hover:bg-primary hover:bg-opacity-[10%] transition-all'>Learn More</Link>
+                        <Link to='/about' className='px-4 py-2 border-primary border text-primary rounded-full hover:bg-primary hover:bg-opacity-[10%] transition-all'>About US</Link>
                     </div>
                 </div>
                 {/* <img className='' src={zulkaif} alt="about_image" /> */}
